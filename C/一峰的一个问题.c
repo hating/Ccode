e99ce349d315dd4e0bb2cbbf8a0f82952b9ff728
Hating <286762857@qq.com>
2015-05-11 00:21:05 +0800

    Update

--- C/一峰的一个问题.c
+++ C/一峰的一个问题.c
@@ -2,21 +2,33 @@
 #include<math.h>
 int main()
 {
-	float x,m,n,a;
-	printf("Please input a int:");
-	scanf("%f",&x);
-//	while(fabs(m-n)>=1e-5)
-//	{
-//		n=x;
-//		m=n+a/n;
-//		x=(1/2)*m;
-//	}
-	do{
+	int a;
+	double b,c;
+	scanf("%d",&a);
+	b=1,c=0;
+	for(;fabs(b-c)>=1e-5;)
+	{
+	    c=1.0/2*(b+a*1.0/b);
+	    b=1.0/2*(c+a*1.0/c);
+	}
+	printf("%lf,%lf",c,b);
+	return 0;
+}
+
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+	float x=0,m=0,n=0,a;
+	printf("请输入一个正数：");
+	scanf("%f",&a);
+	do
+	{
 		n=x;
-		m=n+x/n;
-		x=0.5*m;		
-	} while(fabs(m-n)>=0.00001);
-	printf("The result is:");
+		m=n+a/n;
+		x=(1/2)*m;
+	}while(fabs(m-n)>=1e-5);
+	printf("平方根为：");
 	printf("%8.6f\n",x);
 	return 0;
 }
